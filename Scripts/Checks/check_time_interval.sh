fa96e881d181288ccfc7e44224a1b6fb83c3bb10
Marcos <30581037+marcosvlt@users.noreply.github.com>
2021-11-16 18:27:20 -0300

    Create check_time_interval.sh

---
+++ Scripts/Checks/check_time_interval.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Script to check numbers of databases
+# Author https://github.com/marcosvlt
+# 11/2020 - V.0.1
+# ###################################
+
+HOUR=$(date +%H)
+
+echo "$HOUR"
+
+if [ "$HOUR" -ge 10 ] && [ "$HOUR" -le 23 ]; then
+
+        echo "Between 10 and 23"
+
+else
+
+        echo " Between 00 and 09"
+
+fi
