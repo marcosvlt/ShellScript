9fc3d6241a1681d0e8f062c31d43f0adfc864370
Marcos <30581037+marcosvlt@users.noreply.github.com>
2023-09-10 15:31:17 -0300

    Update backup_cron_tar.sh

--- Scripts/Backups/backup_cron_tar.sh
+++ Scripts/Backups/backup_cron_tar.sh
@@ -3,7 +3,6 @@
 #
 # Automatic backup script. 
 # Used with cron
-# need install notify-send to receibe desktop notification
 # Example: ./backupRsync.sh ORIGEM DESTINATION NAME
 #
 ###########################
@@ -11,10 +10,10 @@
 ORIGEM=$1
 DESTINO=$2
 NOME=$3
-LOG_FILE=(/var/log/backup/$NOME.log)
+LOG_FILE=(/var/log/backup/"$NOME.log")
 
 mkdir /var/log/backup >> /dev/null 2>&1
-touch $LOG_FILE
+touch "$LOG_FILE"
 
 notify-send "Backup '$ORIGEM' iniciado"
 
@@ -22,28 +21,28 @@ if [ "$ORIGEM" == "" -o "$DESTINO" == "" -o "$NOME" == "" ]; then
         echo "Ferramenta de backup"
         echo "Uso Origem Destino Nome"
         echo -e "Erro: Script sem argumentos\nUso Origem Destino Nome\nBackup NOK
-        " >  $LOG_FILE
+        " >  "$LOG_FILE"
         exit 1
   fi
 
-if [ ! -d $ORIGEM ]; then
+if [ ! -d "$ORIGEM" ]; then
 
-        echo "Erro:Pasta de origem \"$ORIGEM\" inexistente" > $LOG_FILE
+        echo "Erro:Pasta de origem \"$ORIGEM\" inexistente" > "$LOG_FILE"
         exit 1
 fi
 
-if [ ! -d $DESTINO ]; then
-        echo "Erro: Pasta de destino \"$DESTINO\" inexistente" > $LOG_FILE
+if [ ! -d "$DESTINO" ]; then
+        echo "Erro: Pasta de destino \"$DESTINO\" inexistente" > "$LOG_FILE"
         exit 1
 fi
 
-rsync -avP --delete $ORIGEM $DESTINO  > $LOG_FILE 2>&1
+rsync -avP --delete "$ORIGEM" "$DESTINO"  > "$LOG_FILE" 2>&1
         if [[ $? = 0 ]]; then
-                echo "Backup OK" >> $LOG_FILE
-                notify-send "Backup '$ORIGEM' Finalizado"
+                echo "Backup OK" >> "$LOG_FILE"
+		notify-send "Backup '$ORIGEM' Finalizado"
         else
-                echo "Backup NOK" >> $LOG_FILE
-                notify-send "Erro ao fazer backup '$ORIGEM'"
+                echo "Backup NOK" >> "$LOG_FILE"
+		notify-send "Erro ao fazer backup '$ORIGEM'"
         fi
 
-date >> $LOG_FILE
+date >> "$LOG_FILE"
